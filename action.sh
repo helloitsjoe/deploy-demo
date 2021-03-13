fa4b564468b3dcec049603c811217c7cbd86bb90
helloitsjoe <jboyle617@gmail.com>
2021-03-13 15:43:58 -0500

    build and push

--- action.sh
+++ action.sh
@@ -6,7 +6,16 @@ build_dir=$3
 target_dir=$4
 
 echo "I am a deploy robot beep boop"
-echo "github_token: $1"
-echo "project_dir: $2"
-echo "build_dir: $3"
-echo "target_dir: $4"
+echo "github_token: $github_token"
+echo "project_dir: $project_dir"
+echo "build_dir: $build_dir"
+echo "target_dir: $target_dir"
+
+yarn
+yarn build
+
+mv ${build_dir} ${target_dir}
+
+git add ${target_dir}
+git commit -m 'Deploy :rocket:'
+git push
