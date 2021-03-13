6e55cb787ddb4ce86b28a44b3fdff5103842f47c
helloitsjoe <jboyle617@gmail.com>
2021-03-13 15:31:44 -0500

    add react

--- action.sh
+++ action.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+github_token=$1
+project_dir=$2
+build_dir=$3
+target_dir=${4:-GITHUB_WORKSPACE}
+
 echo "I am a deploy robot beep boop"
 echo "github_token: $1"
+echo "project_dir: $2"
+echo "build_dir: $3"
+echo "target_dir: $4"
