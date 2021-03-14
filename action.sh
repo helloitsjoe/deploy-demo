23f4121d99b5ae0b170b5546f0303d0c38792eb8
helloitsjoe <jboyle617@gmail.com>
2021-03-13 21:59:25 -0500

    env in docker?

--- action.sh
+++ action.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-github_token=$1
-build_dir=$2
-target_dir=$3
+# github_token=$1
+# build_dir=$2
+# target_dir=$3
 
 echo "I am a deploy robot beep boop"
-echo "github_token: $github_token"
-echo "build_dir: $build_dir"
-echo "target_dir: $target_dir"
+# echo "github_token: $github_token"
+echo "build_dir: $BUILD_DIR"
+echo "target_dir: $TARGET_DIR"
 
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@bots.github.com"
@@ -15,9 +15,9 @@ git config user.email "${GITHUB_ACTOR}@bots.github.com"
 yarn
 yarn build
 
-rm -rf ${target_dir}
-mv ${build_dir} ${target_dir}
+rm -rf ${TARGET_DIR}
+mv ${BUILD_DIR} ${TARGET_DIR}
 
-git add ${target_dir}
+git add ${TARGET_DIR}
 git commit -m 'Deploy :rocket:'
 git push
